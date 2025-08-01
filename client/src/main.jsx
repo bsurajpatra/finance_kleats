30a3245e6523a2f9651cec52edf210eaa5862fd7
bsurajpatra <ankitsuraj1111@gmail.com>
2025-08-01 10:43:31 +0530

    chore: disable console logs in production using import.meta.env.PROD

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.debug = () => {};
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
