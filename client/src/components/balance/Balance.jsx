f64f2f669fa0eb590c020c10b29d8f41f0723edc
Gundumi-Sri-Krishna-Sudhindra <srikrishnasudheendra@gmail.com>
2025-08-02 07:02:39 +0530

    Resizing of header in balance component

--- client/src/components/balance/Balance.jsx
+++ client/src/components/balance/Balance.jsx
@@ -146,7 +146,7 @@ const Balance = () => {
       </div>
 
       <div className="recent-transactions">
-        <div className="transactions-header">
+        <div className="transactions-header-left">
           <h3>
             <svg viewBox="0 0 24 24" fill="currentColor" className="header-icon">
               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
@@ -204,7 +204,7 @@ const Balance = () => {
       </div>
 
       <div className="recent-payouts">
-        <div className="payouts-header">
+        <div className="payouts-header-left">
           <h3>
             <svg viewBox="0 0 24 24" fill="currentColor" className="header-icon">
               <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
